8909c31ba6c60535352dcf9df1d33683bea6d432
Suyeong RHIE <10545351+noxowl@users.noreply.github.com>
2023-01-03 16:33:53 +0900

    modify error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::path::PathBuf;
 use std::ffi::{CStr, CString, c_int};
-use anyhow::{anyhow, Result, Error};
+use thiserror::Error;
 use opencv::core::{in_range, Point, Rect, Scalar, Size};
 use opencv::prelude::*;
 use opencv::imgcodecs::{imread, IMREAD_UNCHANGED, imwrite};
@@ -9,35 +9,54 @@ use opencv::imgproc::{COLOR_BGR2GRAY, COLOR_BGR2HSV, COLOR_HSV2BGR, cvt_color, d
 use opencv::types::VectorOfVectorOfPoint;
 use tesseract_plumbing::{TessBaseApi, Text};
 
+#[derive(Debug, Error, PartialEq)]
+pub enum MaskMyNameError {
+    #[error("Read image failed.")]
+    ImageReadError(),
+    #[error("Mask text failed.")]
+    MaskTextError(),
+    #[error("Failed to initialize Tesseract.")]
+    TessInitError(),
+    #[error("Failed to get text from Tesseract.")]
+    TessGetTextError(),
+    #[error("Failed to create black bar Mat.")]
+    BlackBarCreationError(),
+    #[error("No matching string found.")]
+    NoMatchingString(),
+}
 
-fn load_image(image_path: PathBuf) -> Result<Mat> {
-    match imread(image_path.to_str().unwrap(), IMREAD_UNCHANGED) {
+fn load_image(image_path: PathBuf) -> Result<Mat, MaskMyNameError> {
+    match imread(image_path.to_str().expect("failed to convert PathBuf to str."), IMREAD_UNCHANGED) {
         Ok(image) => {
             Ok(image)
         },
-        Err(e) => {
-            Err(anyhow!("read image failed!"))
+        Err(_) => {
+            Err(MaskMyNameError::ImageReadError())?
         }
     }
 }
 
-fn mask_text(image: &Mat) -> Result<Mat> {
+fn mask_text(image: &Mat) -> Result<Mat, MaskMyNameError> {
     let mut image_hsv: Mat = Default::default();
     let mut image_mask: Mat = Default::default();
-    cvt_color(image, &mut image_hsv, COLOR_BGR2HSV, 0)?;
-    in_range(&image_hsv, &Scalar::new(0., 0., 0., 0.), &Scalar::new(0., 0., 150., 255.), &mut image_mask)?;
-    let kernel = get_structuring_element(MORPH_RECT, Size::new(5, 3), Point::new(-1, -1)).unwrap();
+    cvt_color(image, &mut image_hsv, COLOR_BGR2HSV, 0).expect("Convert BGR2HSV failed. check input image file.");
+    in_range(&image_hsv,
+             &Scalar::new(0., 0., 0., 0.),
+             &Scalar::new(0., 0., 150., 255.),
+             &mut image_mask).expect("in_range failed. check converted Mat is in HSV Colour space.");;
+    let kernel = get_structuring_element(MORPH_RECT, Size::new(5, 3), Point::new(-1, -1)).expect("failed to get_structuring_element.");
     let mut image_dst: Mat = Default::default();
-    dilate(&image_mask, &mut image_dst, &kernel, Point::new(-1, -1), 5, 0, morphology_default_border_value()?).unwrap();
+    dilate(&image_mask, &mut image_dst, &kernel, Point::new(-1, -1), 5, 0,
+           morphology_default_border_value().expect("Failed to calling morphology_default_border_value.")).expect("Failed to calling dilate.");
     Ok(image_dst)
 }
 
-fn find_textarea_from_mask(image: &Mat) -> Result<Vec<Rect>> {
+fn find_textarea_from_mask(image: &Mat) -> Result<Vec<Rect>, MaskMyNameError> {
     let mut contours: VectorOfVectorOfPoint = Default::default();
     let mut rect_result: Vec<Rect> = Default::default();
-    find_contours(image, &mut contours, RETR_EXTERNAL, CHAIN_APPROX_NONE, Default::default()).unwrap();
+    find_contours(image, &mut contours, RETR_EXTERNAL, CHAIN_APPROX_NONE, Default::default()).expect("find_contours failed. ");
     for contour in contours {
-        let rect = bounding_rect(&contour).unwrap();
+        let rect = bounding_rect(&contour).expect("");
         if rect.height < rect.width && rect.height > 15 {
             if rect.width / rect.height < 5 && rect.width < (image.cols() / 2) {
                 rect_result.push(rect);
@@ -47,52 +66,58 @@ fn find_textarea_from_mask(image: &Mat) -> Result<Vec<Rect>> {
     Ok(rect_result)
 }
 
-fn scan_image(tess: &mut TessBaseApi, image: &Mat) -> Result<Text> {
-    tess.set_image(image.data_bytes().unwrap(),
+fn scan_image(tess: &mut TessBaseApi, image: &Mat) -> Result<Text, MaskMyNameError> {
+    tess.set_image(image.data_bytes().expect("Failed to get data_bytes from image Mat."),
                    image.cols() as c_int,
                    image.rows() as c_int,
-                   image.channels(), (image.cols() * image.channels()) as c_int).expect("TODO: panic message");
+                   image.channels(), (image.cols() * image.channels()) as c_int).expect("Set image to Tesseract failed.");
     match tess.get_utf8_text() {
         Ok(text) => { Ok(text) },
-        Err(e) => { Err(anyhow!("scan image failed!")) }
+        Err(e) => { Err(MaskMyNameError::TessGetTextError()) }
     }
 }
 
-fn init_tess(lang: &CStr) -> Result<TessBaseApi> {
+fn black_bar(roi: &Mat) -> Result<Mat, MaskMyNameError> {
+    match Mat::new_rows_cols_with_default(roi.rows(), roi.cols(), roi.typ(), Scalar::new(0., 0., 0., 255.)) {
+        Ok(mat) => { Ok(mat) }
+        Err(_) => { Err(MaskMyNameError::BlackBarCreationError()) }
+    }
+}
+
+fn init_tess(lang: &CStr) -> Result<TessBaseApi, MaskMyNameError> {
     let mut ocr = TessBaseApi::create();
     match ocr.init_2(None, Some(lang)) {
         Ok(_) => { Ok(ocr) },
-        Err(e) => { Err(anyhow!("init tesseract failed!")) }
+        Err(e) => { Err(MaskMyNameError::TessInitError()) }
     }
 }
 
-fn mask_my_name(image_path: PathBuf, target_string: String) -> Result<()> {
+fn mask_my_name(lang: CString, image_path: PathBuf, target_string: String) -> Result<Mat, MaskMyNameError> {
+    let mut success = false;
     let mut image = load_image(image_path)?;
     let mut target_image: Mat = Default::default();
-    let lang = CString::new("eng")?;
     match init_tess(lang.as_c_str()) {
         Ok(mut tess) => {
             for area in find_textarea_from_mask(&mask_text(&image)?)? {
-                let mut roi = Mat::roi(&image, area).unwrap();
-                roi.copy_to(&mut target_image).unwrap();
+                let mut roi = Mat::roi(&image, area).expect("Failed to create ROI.");
+                roi.copy_to(&mut target_image).expect("Failed to copy roi data.");
                 match scan_image(&mut tess, &target_image) {
                     Ok(text) => {
-                        if text.as_ref().to_str()?.to_lowercase().contains(&target_string.to_lowercase()) {
-                            Mat::new_rows_cols_with_default(roi.rows(), roi.cols(), roi.typ(), Scalar::new(0., 0., 0., 255.)).unwrap().copy_to(&mut roi)?;
-                            imwrite("output.jpg", &image, &Default::default()).unwrap();
+                        if text.as_ref().to_str().unwrap_or("").to_lowercase().contains(&target_string.to_lowercase()) {
+                            success = true;
+                            black_bar(&roi)?.copy_to(&mut roi).expect("Failed to copy black bar data.");
                         }
                     },
-                    Err(_) => {
-
-                    }
+                    Err(e) => { return Err(e); }
                 }
             }
+            match success {
+                true => { Ok(image) },
+                false => { Err(MaskMyNameError::NoMatchingString()) }
+            }
         },
-        Err(e) => {
-
-        }
+        Err(e) => { Err(e) }
     }
-    Ok(())
 }
 
 struct Cli {
@@ -100,12 +125,26 @@ struct Cli {
     target_string: String
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = env::args().nth(1).expect("no path given");
     let target_string = env::args().nth(2).unwrap_or("".to_string());
     let args = Cli {
         image_path: PathBuf::from(path),
         target_string,
     };
-    mask_my_name(args.image_path, args.target_string).unwrap();
+    match mask_my_name(CString::new("eng")?, args.image_path, args.target_string) {
+        Ok(image) => {
+            println!("Matching found. write masked image to disk.");
+            imwrite("output.jpg", &image, &Default::default()).expect("Failed to write image data.");
+        },
+        Err(e) => {
+            match e {
+                MaskMyNameError::NoMatchingString() => {
+                    println!("{}", e);
+                }
+                _ => {}
+            }
+        }
+    }
+    Ok(())
 }
